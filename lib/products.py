5923b4a26cf919ba0bc1e199cf2b2c4b4b54daf9
teacher <itstep.codeclub@gmail.com>
2020-06-23 20:11:20 +0300

    Изменен файл products.py - добавлены классы транспортных средств

--- lib/products.py
+++ lib/products.py
@@ -1,14 +1,57 @@
-class Transport(object):
-    pass
+from abc import ABC, abstractmethod
+
+
+class Transport(ABC):
+
+	def __init__(self, ttype: str, tmodel: str, tweight: float):
+		self._ttype = ttype
+		self._tmodel = tmodel
+		self._tweight = tweight
+
+	def __str__(self) -> str:
+		return f'{self._ttype} - {self._tmodel} / {self._tweight} tonn'
+    
+    @abstractmethod
+    def deliver():
+    	pass
 
 
 class Truck(Transport):
-    pass
+    
+    def __init__(self, ttype: str, tmodel: str, tweight: float, height: float):
+    	super().__init__(ttype, tmodel, tweight)
+    	self._height = height
+
+    def __str__(self) -> str:
+    	return super().__str__() + f'; height = {self._height} m'
+
+    def deliver():
+    	print('Перевозка по шоссейным дорогам в пределах Европы')
 
 
 class Ship(Transport):
-    pass
+    
+    def __init__(self, ttype: str, tmodel: str, tweight: float, draft: float):
+    	super().__init__(ttype, tmodel, tweight)
+    	self._draft = draft
+
+    def __str__(self) -> str:
+    	return super().__str__() + f'; draft = {self._draft} m'
+
+    def deliver():
+    	print('Перевозка по Днепру и судоходным притокам')
+
 
 
 class AirPlane(Transport):
-    pass
+    
+    def __init__(self, ttype: str, tmodel: str, tweight: float, distance: float):
+    	super().__init__(ttype, tmodel, tweight)
+    	self._distance = distance
+
+    def __str__(self) -> str:
+    	return super().__str__() + f'; distance = {self._distance} km'
+
+    def deliver():
+    	print('Авиаперевозки по региональным авиалиниям')
+
